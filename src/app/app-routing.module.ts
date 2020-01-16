42cbdf89bb4b89b14d225ac22ebce6b5d67f686d
Tara Z. Manicsic <tzmanics@gmail.com>
2020-01-16 14:44:35 -0500

    adds routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => import("./home/home.module").then(m => m.HomeModule)
+  },
+  {
+    path: "page1",
+    loadChildren: () => import("./page1/page1.module").then(m => m.Page1Module)
+  },
+  {
+    path: "page2",
+    loadChildren: () => import("./page2/page2.module").then(m => m.Page2Module)
+  },
+  {
+    path: "page3",
+    loadChildren: () => import("./page3/page3.module").then(m => m.Page3Module)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
