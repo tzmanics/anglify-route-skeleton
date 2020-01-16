d49651673a1810b9bc123c8c00735783e658df28
Tara Z. Manicsic <tzmanics@gmail.com>
2020-01-16 14:54:54 -0500

    adds header and footer comps and moves pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,23 @@ import { Routes, RouterModule } from "@angular/router";
 const routes: Routes = [
   {
     path: "",
-    loadChildren: () => import("./home/home.module").then(m => m.HomeModule)
+    loadChildren: () =>
+      import("./components/home/home.module").then(m => m.HomeModule)
   },
   {
     path: "page1",
-    loadChildren: () => import("./page1/page1.module").then(m => m.Page1Module)
+    loadChildren: () =>
+      import("./components/page1/page1.module").then(m => m.Page1Module)
   },
   {
     path: "page2",
-    loadChildren: () => import("./page2/page2.module").then(m => m.Page2Module)
+    loadChildren: () =>
+      import("./components/page2/page2.module").then(m => m.Page2Module)
   },
   {
     path: "page3",
-    loadChildren: () => import("./page3/page3.module").then(m => m.Page3Module)
+    loadChildren: () =>
+      import("./components/page3/page3.module").then(m => m.Page3Module)
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
